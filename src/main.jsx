30626fc71d4e9d1774ed9d5cc541127827872071
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-02 11:58:50 +0600

    base setup and route setup

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout/Layout.jsx";
+import Error from "./pages/Error.jsx";
+import Home from "./pages/Home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
